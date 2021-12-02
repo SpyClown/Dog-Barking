74dedf296c7bf4719af46078046a76d0767e510d
SpyClown <95299605+SpyClown@users.noreply.github.com>
2021-12-02 16:10:29 +0100

    Adding Database con and separeting the sound
    Installing Phpmyadmin and setting up for the Raspberry and for the script:
    - Creating Table and Colums for it (ID, Date , Barking).
    - Importing Mysql.connector to making the connection.
    - Making an INSERT command to get the datas into the database.
    - Importing the time to knowing whats the time and formating it and inserting to the database.
    - Importing random numbers and using random number for id because its not incrasing automatically.
    - Making a Queary where i try to get the last 5 records and checking the dog barked or not.
    
    Problems:
    Still not stable, about knowing why sometimes choosing the not barking part even there is bark.
    I can not make and use simple variable which is outside from the def.

--- sound_checker.py
+++ sound_checker.py
@@ -1,20 +1,65 @@
 import RPi.GPIO as GPIO
 import time
+import mysql.connector
+import random
+from datetime import datetime
 
-#GPIO SETUP
+db = mysql.connector.connect(
+	host="localhost",
+	user="root",
+	passwd="root",
+	database="dog_barking"
+	)
+
+mycursor = db.cursor()
+
+#GPIO Setuping part
+
+ID = 1
 channel = 17
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(channel, GPIO.IN)
 
 def callback(channel):
-        if GPIO.input(channel):
-                print "Sound Detected!"
-        else:
-                print "Sound Not Detected!"
 
-GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
-GPIO.add_event_callback(channel, callback)  # assign function to GPIO PIN, Run function on change
+	if GPIO.input(channel):
+		print "Sound Detected!"
+		now = datetime.now()
+		print now
+		dt_string = now.strftime("%Y-%m-%d %H:%M:%S")
+		print dt_string
+		n = random.randint(0,2500000)
+		print n
+		ugat = 1
+		mennyi = 0
+		egy = 1
+		sql = "INSERT INTO Ugatasok (ID, Date, Ugat) VALUES (%s, %s, %s)"
+		val = (n ,dt_string, ugat)
+		mycursor.execute(sql, val)
+		db.commit()
+		lekerdezes = "SELECT * FROM Ugatasok ORDER BY Date DESC LIMIT 5"
+		mycursor.execute(lekerdezes)
+		records = mycursor.fetchall()
+		for record in records:
+			if egy in record:
+				mennyi += 1
+				print "Ez a mennyi erteke"
+				print mennyi
+		if(mennyi == 5):
+			print "Siker!" 
+	else:
+		n = random.randint(0,2500000)
+		now = datetime.now()
+		dt_string = now.strftime("%Y-%m-%d %H:%M:%S")
+		ugat = 0
+		print "Sound not Detected!"
+		sql = "INSERT INTO Ugatasok (ID, Date, Ugat) VALUES (%s, %s, %s)"
+		val = (n, dt_string, ugat)
+		mycursor.execute(sql,val)
+		db.commit()
+GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)
+GPIO.add_event_callback(channel, callback)
 
-# infinite loop
 while True:
-        time.sleep(1)
+	time.sleep(1)
+
